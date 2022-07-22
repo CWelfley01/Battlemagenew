171898656529e78cad8614d945bcf47444e9e12f
cwelfley01 <cwelfley01@gmail.com>
2022-07-21 23:54:59 -0400

    hub styles

--- src/components/hud.js
+++ src/components/hud.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import styles from "../style/hud.scss";
 
 import Scroll from "../../static/assets/images/blank-scroll-png.png";
-import EmptySpell from "../../static/assets/images/emptySpellForm.png";
+import EmptySpell from "./emptyspell.js";
 import Fire from "./fire.js";
 import Water from "./water.js";
 import Air from "./air.js";
@@ -11,16 +11,16 @@ import Earth from "./earth.js";
 export default class Hud extends Component {
   render() {
     return (
-      <div>
-        <div className="background">
-          <img src={Scroll} />
-        </div>
-
-        <div className="hud">
-          <div className="spellform">
-            <img src={EmptySpell} />
+      <div className="content-wrapper">
+        <div className="spellform">
+          <div className="background">
+            <img src={Scroll} />
           </div>
-
+          <div className="foreground">
+            <EmptySpell />
+          </div>
+        </div>
+        <div className="hud">
           <div className="mana">
             <button>
               <Fire />
